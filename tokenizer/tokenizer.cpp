a81b203aaae7a912cf898755213d2e271d4de613
jh3141 <jh314159@googlemail.com>
2015-02-16 11:24:50 +0000

    Remove unnecessary code from testing build setup

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -13,9 +13,5 @@ namespace kind
             return Token(Token::Type::T_EOF);
         }
         
-        void printVersion (std::ostream & out)
-        {
-            out << "Kind Tokenizer build " __DATE__ " " __TIME__ << std::endl;
-        }
     }
 }
